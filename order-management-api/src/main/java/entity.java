781588bdbf742a441c37a503e917b99604568441
Micaela Tamires <micaela.tamir@gmail.com>
2026-03-26 15:10:38 -0300

    feat(usuario): iniciar estrutura da entidade User feat(user): start User entity structure

--- order-management-api/src/main/java/entity.java
+++ order-management-api/src/main/java/entity.java
@@ -1,2 +1,21 @@
-public class entity {
+import java.time.localDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@entity
+public class User{
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+
+
+
 }

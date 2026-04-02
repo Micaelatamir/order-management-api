a70d33538f4a213c6a96671d1f7f3a5207b9899f
Micaela Tamires <micaela.tamir@gmail.com>
2026-04-02 19:18:36 -0300

    iniciando o model;
    Starting the model

--- order-management-api/src/main/java/com/micaela/ordermanagementapi/model/Order.java
+++ order-management-api/src/main/java/com/micaela/ordermanagementapi/model/Order.java
@@ -1,4 +1,28 @@
 package com.micaela.ordermanagementapi.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order")
 public class Order {
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable= false, unique= true)
+    private int price;
+
+    public Order () {
+    }
+    public Order (id, name, price){
+        this.id = id;
+        this.name = name;
+        this.price = price ;
+    }
+    @PrePersist
 }
+

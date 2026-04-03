90430e2b2d6271632b06fa035460bde87c6989f1
Micaela Tamires <micaela.tamir@gmail.com>
2026-04-03 00:05:06 -0300

    feat: criando entidade OrderItem com relacionamento para Order e Product
    feat: creating an OrderItem entity with a relationship to Order and Product

--- order-management-api/src/main/java/com/micaela/ordermanagementapi/model/OrderItem.java
+++ order-management-api/src/main/java/com/micaela/ordermanagementapi/model/OrderItem.java
@@ -1,4 +1,65 @@
 package com.micaela.ordermanagementapi.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "order_items")
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal total;
+
+
+    public OrderItem(){}
+
+
+    public OrderItem(Order order, Product product, Integer quantity, BigDecimal price ) {
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+        this.total = total ;
+
+    }
+
+    public Long getId() {]return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public Order getOrder() {return order;}
+
+    public void setOrder(Order order) {this.order = order;}
+
+    public Product getProduct() {return product;}
+
+    public void setProduct(Product product) {this.product = product;}
+
+    public Integer getQuantity() {return quantity;}
+
+    public void setQuantity(Integer quantity) {this.quantity = quantity;}
+
+    public BigDecimal getTotal() { return total; }
+    public void setTotal(BigDecimal total) { this.total = total; }
+
+
 }
+
+

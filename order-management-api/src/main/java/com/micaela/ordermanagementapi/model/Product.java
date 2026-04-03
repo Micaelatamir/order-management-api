cd705886f14452bcb6a98a089e9dfcb1f619c674
Micaela Tamires <micaela.tamir@gmail.com>
2026-04-02 23:18:24 -0300

    feat: criando entidade Product com mapeamento JPA
    feat: Creating a Product entity with JPA mapping

--- order-management-api/src/main/java/com/micaela/ordermanagementapi/model/Product.java
+++ order-management-api/src/main/java/com/micaela/ordermanagementapi/model/Product.java
@@ -1,4 +1,43 @@
 package com.micaela.ordermanagementapi.model;
+import java.util.List;
+import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "product")
+    private List<Product> items;
+
+    public Product() {
+    }
+
+    public Product( String name, BigDecimal price){
+        this.name = name;
+        this.price = price ;
+    }
+    //Getters e Setters
+
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public BigDecimal getPrice() {return price;}
+
+    public void setPrice(BigDecimal price) {this.price = price;}
 }

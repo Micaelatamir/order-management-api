4274ba6aae1379482a4951c4a2e72f9996ff3118
Micaela Tamires <micaela.tamir@gmail.com>
2026-04-03 17:08:21 -0300

    feat: criando repositories para User, Product, Order e OrderItem
    feat: creating repositories for User, Product, Order, and OrderItem

--- order-management-api/src/main/java/com/micaela/ordermanagementapi/repository/UserRepository.java
+++ order-management-api/src/main/java/com/micaela/ordermanagementapi/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.micaela.ordermanagementapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.micaela.ordermanagementapi.model.User;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }

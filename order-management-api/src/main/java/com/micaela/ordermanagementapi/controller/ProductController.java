f3a1d7865c964002d7a0e8d05517322b39bb5a33
Micaela Tamires <micaela.tamir@gmail.com>
2026-04-04 23:54:00 -0300

    feat: criando controllers para User e Product com endpoints REST
    feat: Creating controllers for User and Product with REST endpoints

--- order-management-api/src/main/java/com/micaela/ordermanagementapi/controller/ProductController.java
+++ order-management-api/src/main/java/com/micaela/ordermanagementapi/controller/ProductController.java
@@ -1,4 +1,37 @@
 package com.micaela.ordermanagementapi.controller;
+import com.micaela.ordermanagementapi.model.Product;
+import com.micaela.ordermanagementapi.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
-public class ProductController {
+
+@RestController
+@RequestMapping("/products")
+public class ProductController{
+
+
+@Autowired
+private ProductService productService;
+
+
+@GetMapping
+public List<Product> findAll(){
+    return productService.findAll();
+}
+
+@GetMapping("/{id}")
+public Product findByid(@PathVariable Long id){
+    return productService.findById(id);
+}
+
+@PostMapping
+public Product save(@RequestBody Product product) {
+    return productService.save(product);
+}
+
+@DeleteMapping("/{id}")
+public void delete(@PathVariable Long id) {
+    productService.delete(id);
+}
 }

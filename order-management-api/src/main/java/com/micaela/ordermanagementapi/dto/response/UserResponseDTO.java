8d6210a61548584f566c79fe2e82a3c5a5b2473a
Micaela Tamires <micaela.tamir@gmail.com>
2026-04-07 19:23:17 -0300

    feat: criando DTOs de request e response para User
    feat: creating request and response DTOs for User

---
+++ order-management-api/src/main/java/com/micaela/ordermanagementapi/dto/response/UserResponseDTO.java
@@ -0,0 +1,34 @@
+package com.micaela.ordermanagementapi.dto.response;
+import com.micaela.ordermanagementapi.model.User;
+import java.time.LocalDateTime;
+
+public class UserResponseDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+
+    public UserResponseDTO(User user){
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.createdAt = user.getCreatedAt();
+    }
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public LocalDateTime getCreatedAt() {return createdAt;}
+
+    public void setCreatedAt(LocalDateTime createdAt) {this.createdAt = createdAt;}
+}
